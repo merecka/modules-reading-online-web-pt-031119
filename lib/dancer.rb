371cedf8b38dc44799b18f58040a9b3edbb60ec1
Alex Merecka <merecka@gmail.com>
2019-04-13 22:59:43 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,11 +5,11 @@ class Dancer
 
   extend MetaDancing
 
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  # include Dance
+  # 
+  # attr_accessor :name
+  # 
+  # def initialize(name)
+  #   @name = name
+  # end
 end
